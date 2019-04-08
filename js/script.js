53d3157367625edd8b8848f41577fdd6ccf90455
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-08 14:23:24 +0300

    Introduced constructors and prototypes

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ function Player() {
   this.currentScore = 0;
   this.cumulativeScore = 0;
 }
+function newTurn(player){
+  player.roll = 0;
+  player.currentScore = 0;
+
+  $('#tempScore').html(player.roll);
+  $('#currentScore').html(player.currentScore);
+  $('#totalScore').html(player.cumulativeScore);
+}
+Player.prototype.checkGameWinner = function () {
+  if (this.cumulativeScore >= 100) {
+    alert(this.name + " Wins the game");
+    endGame();
+  }
+}
+
+Player.prototype.checkOne = function () {
+  if(this.roll === 1){
+    alert('You rolled a 1. Your turn is over');
+    this.roll == 0;
+    this.holdGame();
+  }
+}
