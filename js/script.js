9097bdb67b16579d58a2b3ff81817f5e99845bac
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-08 11:45:58 +0300

    Added logic functionalities of the game

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
-<script>
-function roll ()
-{
+var sum1 = 0;
+var sum2 = 0;
+var rollNumber1 = 0;
+var rollNumber2 = 0;
+var rollSum = 0;
+
+function roll (){
   var roll1, roll2
   roll1 = Math.floor(Math.random()*6) +1;
-  alert ('You rolled a '+ roll1);
+
   roll2 = Math.floor(Math.random()*6) +1;
-  alert ('You rolled a '+ roll2);
 }
+function sum1(){
+  return rollNumber1 += getRandomIntInclusive (1,6);
+  return rollNumber1;
+}
+function addition1(){
+  return sum1 = sum1 + rollNumber1;
+}
+function sum2(){
+  return rollNumber2 += getRandomIntInclusive(1,6);
+  return rollNumber2;
+}
+function addition2(){
+  return sum2 = sum2 + rollNumber2;
+}
+
+$(document).ready(function(){
+  $("#button2a").click(function(){
+    $("#outcome2").text(equals);
+    $("#outcome2b").text(addition1());
+  });
+  $("#button3a").click(function(){
+    $("button3a").attr("disabled", true);
+    $("button")
+  });
+});
