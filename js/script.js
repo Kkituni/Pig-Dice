e8cd94fe48303d16d5ee93fb0220033a8c9a201b
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-08 08:29:02 +0300

    Added css and modified js file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 <script>
 function roll ()
 {
-  const roll1, roll2
-  roll1 = (Math.random)
-  roll2 = (Math.random)
+  var roll1, roll2
+  roll1 = Math.floor(Math.random()*6) +1;
+  alert ('You rolled a '+ roll1);
+  roll2 = Math.floor(Math.random()*6) +1;
+  alert ('You rolled a '+ roll2);
 }
