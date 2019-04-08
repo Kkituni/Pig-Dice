844e76e624b0fb00394ee9132395ebd3a6b39cdc
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-08 14:22:05 +0300

    Added initial declarations

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
-var sum1 = 0;
-var sum2 = 0;
-var rollNumber1 = 0;
-var rollNumber2 = 0;
-var rollSum = 0;
+var player1 = "";
+var player2 = "";
 
-function roll (){
-  var roll1, roll2
-  roll1 = Math.floor(Math.random()*6) +1;
-
-  roll2 = Math.floor(Math.random()*6) +1;
-}
-function sum1(){
-  return rollNumber1 += getRandomIntInclusive (1,6);
-  return rollNumber1;
-}
-function addition1(){
-  return sum1 = sum1 + rollNumber1;
-}
-function sum2(){
-  return rollNumber2 += getRandomIntInclusive(1,6);
-  return rollNumber2;
-}
-function addition2(){
-  return sum2 = sum2 + rollNumber2;
+function singleRoll (){
+    return Math.floor(Math.random()*6) +1;
 }
 
-$(document).ready(function(){
-  $("#button2a").click(function(){
-    $("#outcome2").text(equals);
-    $("#outcome2b").text(addition1());
-  });
-  $("#button3a").click(function(){
-    $("button3a").attr("disabled", true);
-    $("button")
-  });
-});
+function Player() {
+  this.name;
+  this.roll = 0;
+  this.currentScore = 0;
+  this.cumulativeScore = 0;
+}
